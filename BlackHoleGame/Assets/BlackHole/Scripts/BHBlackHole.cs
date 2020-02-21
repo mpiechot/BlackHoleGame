cd06f63ea386c6829e9e9774ae8106d259446774
Tom Haeschke <tom-niklas.haeschke@student.uni-tuebingen.de>
2020-02-21 21:11:36 +0100

    BlackHole

---
+++ BlackHoleGame/Assets/BlackHole/Scripts/BHBlackHole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BHBlackHole : MonoBehaviour
+{
+
+    private void Update()
+    {
+        CheckForAttractedObjects();
+    }
+
+    private void CheckForAttractedObjects()
+    {
+
+    }
+
+    private Vector3 Attract(Transform attractedObject)
+    {
+        return Vector3.zero;
+    }
+}
